9f33d8c2b5c75f2e4a831f9070e766b36317cba6
ericchentch <ericchen.tch@gmail.com>
2023-08-18 10:55:22 +0700

    add comment to req

--- src/service/review-service/review-req.ts
+++ src/service/review-service/review-req.ts
@@ -1,12 +1,24 @@
-import { IS_EMAIL, IS_NUMBER, IS_REQUIRED, ObjectValidator } from 'common-abstract-fares-system'
+import {
+  IS_EMAIL,
+  IS_NUMBER,
+  IS_PHONE,
+  IS_REQUIRED,
+  ObjectValidator,
+} from 'common-abstract-fares-system'
+
 import { IS_PRODUCT_ID } from './custom-validation/IS_PRODUCT_ID'
+import { ReviewVariantType } from '@/src/repository/review-repository/review-entity'
 
 export class ReviewRequest {
   productId: string = ''
 
-  variantId: string[] = []
+  variantId: { id: string; type: ReviewVariantType }[] = []
 
   email: string = ''
+
+  comment: string = ''
+
+  phone: string = ''
 
   name: string = ''
 
@@ -17,6 +29,8 @@ export const ReviewReqValidator: ObjectValidator<ReviewRequest> = {
   productId: IS_PRODUCT_ID,
   variantId: IS_REQUIRED,
   email: IS_EMAIL,
+  phone: IS_PHONE,
+  comment: IS_REQUIRED,
   name: IS_REQUIRED,
   score: IS_NUMBER,
 }
